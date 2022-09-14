440074ad70c71e0a4e99af505b0cb26577f71dd4
abhildevkc <abhildevkc7@gmail.com>
2022-09-14 10:21:52 +0530

    Transfer files

---
+++ Dockeing/src/main/java/com/tcs/docker/DockeingApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockeingApplication.class, args);
+	}
+
+}
